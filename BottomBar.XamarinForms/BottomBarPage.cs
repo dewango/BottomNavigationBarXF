4722d70ae38d843ae5a57fbdeb7b9ac3bf7a84ac
Sebastian Schmidt <sebastian.schmidt@thrivenow.io>
2016-10-23 17:27:17 +0200

    formatted code

--- BottomBar.XamarinForms/BottomBarPage.cs
+++ BottomBar.XamarinForms/BottomBarPage.cs
@@ -21,13 +21,12 @@ namespace BottomBar.XamarinForms
 {
 	public class BottomBarPage : TabbedPage
 	{
-	    public enum BarThemeTypes { Light, DarkWithAlpha, DarkWithoutAlpha }
+		public enum BarThemeTypes { Light, DarkWithAlpha, DarkWithoutAlpha }
 
-        public bool FixedMode { get; set; }
-        public BarThemeTypes BarTheme { get; set; }
+		public bool FixedMode { get; set; }
+		public BarThemeTypes BarTheme { get; set; }
 
-
-        public void RaiseCurrentPageChanged()
+		public void RaiseCurrentPageChanged()
 		{
 			OnCurrentPageChanged();
 		}
