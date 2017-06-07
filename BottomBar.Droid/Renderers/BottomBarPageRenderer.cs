4562e30ea0bad15c5bfceafb8e4020e8b089a1e8
Henning Mosand Stephansen <henningmosand@outlook.com>
2017-06-07 08:26:33 +0200

    Resolves #54: Makes Tab events virtual

--- BottomBar.Droid/Renderers/BottomBarPageRenderer.cs
+++ BottomBar.Droid/Renderers/BottomBarPageRenderer.cs
@@ -48,7 +48,7 @@ namespace BottomBar.Droid.Renderers
 		}
 
 		#region IOnTabClickListener
-		public void OnTabSelected (int position)
+		public virtual void OnTabSelected (int position)
 		{
 			//Do we need this call? It's also done in OnElementPropertyChanged
 			SwitchContent(Element.Children [position]);
@@ -57,7 +57,7 @@ namespace BottomBar.Droid.Renderers
 			//bottomBarPage.RaiseCurrentPageChanged();
 		}
 
-		public void OnTabReSelected (int position)
+		public virtual void OnTabReSelected (int position)
 		{
 		}
 		#endregion
